da5c0ed32b8d670381c79b1a00fa672a06d93e62
FrostyKitten02 <alen.fridau02@gmail.com>
2025-10-19 01:42:29 +0200

    build options

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+    compilerOptions: {
+      dev: false,
+      css: 'external',
+      }
+  })],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
